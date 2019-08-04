4a743c65706e145dade35597ca22ceae3dd4a9c0
kirra93 <kirra.z1993@gmail.com>
2019-08-05 00:13:19 +0300

    fix encoding bug.

--- test2.php
+++ test2.php
@@ -11,7 +11,9 @@ $keysDict = [
     'costs' => [],
     'descriptions' => [],
 ];
-$text =  trim(file_get_contents('data/test2.txt'));
+$text =  trim(encode_utf8(file_get_contents('data/test2.txt')));
+// var_dump($text);
+// die();
 $tmpArray = [];
 preg_match_all($regexDict['articule'], $text,$tmpArray);
 $keysDict['articules'] = $tmpArray[0];
@@ -22,12 +24,23 @@ $keysDict['descriptions'] = $tmpArray[0];
 
 echo toText($keysDict);
 
+function encode_utf8($data) {
+    if ($data === null || $data === '') {
+        return $data;
+    }
+    if (!mb_check_encoding($data, 'UTF-8')) {
+        return iconv(mb_detect_encoding($data, mb_detect_order(), true), "UTF-8", $data);
+    } else {
+        return $data;
+    }
+}
+
 function getText() {
     echo 'Enter filePath: ';
     $handle = fopen ("php://stdin","r");
     $fileName = trim(fgets($handle));
     $file1 = trim(file_get_contents($fileName));
-    if ($file1 === '') {
+    if ($file1 === null || $file1 === '') {
         throw \Exception('File is empty');
     }
     return $file1;
