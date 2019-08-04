dbf9a6d406ff1cdf97e23571d11536abd57a98fc
kirra93 <kirra.z1993@gmail.com>
2019-08-04 22:01:35 +0300

    fix first task (optimize to O(n))

--- test1.php
+++ test1.php
@@ -2,18 +2,29 @@
 
 
     $nums = readNumsFile();
-    $sorted = sort($nums['array']);
-
-    if ($sorted) {
-        echo $nums['string'] . PHP_EOL . ($nums['array'][0] * $nums['array'][1]);
-    } else {
-        echo 'Error';
+    $min = [
+        $nums['array'][0],
+        $nums['array'][1]
+    ];
+    for ($i = 2; $i < count($nums['array']); $i++) {
+        if ($min[0] > $min[1]) {
+            $min[0] += $min[1];
+            $min[1] = $min[0] - $min[1];
+            $min[0] = $min[0] - $min[1];
+        }
+        if ($min[1] > $nums['array'][$i]) {
+            $min[1] = $nums['array'][$i];
+        }
     }
+
+    echo $nums['string'] . PHP_EOL . ($min[0] * $min[1]);
+   
 
     function readNumsFile() {
         echo 'Enter filePath: ';
         $handle = fopen ("php://stdin","r");
         $fileName = trim(fgets($handle));
+        // $fileName = 'data/nums.txt';
         $file1 = trim(file_get_contents($fileName));
         if ($file1 === '') {
             throw \Exception('File is empty');
